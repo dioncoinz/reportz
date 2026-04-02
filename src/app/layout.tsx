79d86e9a242140cbc30eef435c767489ffeee967
dioncoinz <dioncoinz@gmail.com>
2026-04-02 20:05:07 +0800

    Add Reportz tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata = {
   title: "Reportz",
   description: "Post-shutdown reporting",
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
